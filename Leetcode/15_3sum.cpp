b36aec0d6f41d81f40e3559808ed0051ff9950b1
Luis Ayerdi-Morales <luisroayerdi@hotmail.com>
2026-09-05 06:23:14 -0500

    Solution leetcode 15 3 sum

--- Leetcode/15_3sum.cpp
+++ Leetcode/15_3sum.cpp
@@ -6,10 +6,49 @@ class Solution {
 public:
   std::vector<std::vector<int>> threeSum(std::vector<int> &nums) {
     std::vector<std::vector<int>> solution;
-    std::sort(nums.begin(), nums.end());
+    sort(nums.begin(), nums.end());
 
-    // use for loop instead of while loop easier to keep track
+    int n = nums.size();
+    int fixed = 0;
 
+    while (fixed < n - 2) {
+      // skip duplicate 'fixed values
+      if (fixed > 0 && nums[fixed] == nums[fixed - 1]) {
+        fixed++;
+        continue;
+      }
+
+      int left = fixed + 1;
+      int right = n - 1;
+
+      while (left < right) {
+        int sum = nums[fixed] + nums[left] + nums[right];
+
+        if (sum > 0) {
+          right--;
+        } else if (sum < 0) {
+          left++;
+        } else {
+          std::vector<int> triplet;
+          triplet.push_back(nums[fixed]);
+          triplet.push_back(nums[left]);
+          triplet.push_back(nums[right]);
+          solution.push_back(triplet);
+
+          left++;
+          right--;
+
+          while (left < right && nums[left] == nums[left - 1]) {
+            left++;
+          }
+
+          while (left < right && nums[right] == nums[right + 1]) {
+            right--;
+          }
+        }
+      }
+      fixed++;
+    }
     return solution;
   }
 };
@@ -17,7 +56,7 @@ public:
 int main() {
   Solution sol;
 
-  std::vector<int> nums = {-1, 0, 1, 2, -1, -4};
+  std::vector<int> nums = {1, 2, 0, 1, 0, 0, 0, 0};
 
   std::vector<std::vector<int>> result = sol.threeSum(nums);
   for (const auto &triplet : result) {
